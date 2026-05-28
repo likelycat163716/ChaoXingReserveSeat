567ba69f1559b45258c056680c448eda1891c02a
lj7236149-pixel <lj7236149@gmail.com>
2026-05-28 21:36:53 +0800

    更新 __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,8 @@
-import os 
+import os
 from .encrypt import AES_Encrypt, generate_captcha_key, enc, verify_param
-from .reserve import reserve
+from .reserve import SeatReserver, reserve
+from .captcha import CaptchaSolver, CAPTCHA_ID
+
 
 def _fetch_env_variables(env_name, action):
     try:
@@ -9,7 +11,8 @@ def _fetch_env_variables(env_name, action):
         print(f"Environment variable {env_name} is not configured correctly.")
         return None
 
+
 def get_user_credentials(action):
-    usernames = _fetch_env_variables('USERNAMES', action)
-    passwords = _fetch_env_variables('PASSWORDS', action)
+    usernames = _fetch_env_variables("USERNAMES", action)
+    passwords = _fetch_env_variables("PASSWORDS", action)
     return usernames, passwords
