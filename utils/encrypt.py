633579d20b4f19f50343ea20a62c3fca70ee3546
lj7236149-pixel <lj7236149@gmail.com>
2026-05-29 10:35:29 +0800

    创建 encrypt.py

---
+++ utils/encrypt.py
@@ -0,0 +1,36 @@
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.backends import default_backend
+import base64
+from hashlib import md5
+import random
+from uuid import uuid1
+import hashlib
+
+
+def AES_Encrypt(data):
+    key = b"u2oh6Vu^HWe4_AES"  # Convert to bytes
+    iv = b"u2oh6Vu^HWe4_AES"  # Convert to bytes
+    padder = padding.PKCS7(128).padder()
+    padded_data = padder.update(data.encode("utf-8")) + padder.finalize()
+    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
+    encryptor = cipher.encryptor()
+    encrypted_data = encryptor.update(padded_data) + encryptor.finalize()
+    enctext = base64.b64encode(encrypted_data).decode("utf-8")
+    return enctext
+
+
+def resort(submit_info):
+    return {key: submit_info[key] for key in sorted(submit_info.keys())}
+
+
+def enc(submit_info):
+    add = lambda x, y: x + y
+    processed_info = resort(submit_info)
+    needed = [
+        add(add("[", key), "=" + value) + "]" for key, value in processed_info.items()
+    ]
+    pattern = "%sd`~7^/>N4!Q#){''"
+    needed.append(add("[", pattern) + "]")
+    seq = "".join(needed)
+    return md5(seq.encode("utf-8")).hexdigest()
